145a6e30f7086adddb853bbe194f7da714e963f5
hieu1966hn <hieu1966hn@gmail.com>
2022-09-30 20:08:01 +0700

    new code SS1

---
+++ SS1/SS1-Giới thiệu khóa WI.md
@@ -0,0 +1,6 @@
+Web Intensive: Lập trình web chuyên sâu
+
+1. Kiến thức chuyên sâu của Javascript: Xử lý với mảng + cú pháp phức tạp mới nhất Javascript
+2. OOP: Lập trình hướng đối tượng - Kỹ thuật quan trọng và sử dụng nhiều hiện nay
+3. Thực hành xây dựng 1 mini Project MindX Blog (mini social): C,R,U,D
+4. Kiến thức tự học: Responsive (hg dẫn sơ lược)
---
+++ SS1/app.js
@@ -0,0 +1,32 @@
+/*
+HTML: Ngôn ngữ đánh dấu siêu văn bản => trình bày văn bản/nội dung trên web
+CSS: nn trang trí => Cho HTML
+Javascript: nn lập trình 
+=> lý do: Nó có thể làm được rất nhiều tính năng xử lý logic, hoặc thao tác với HTML, CSS
+
+Kiểu dữ liệu cơ bản trong JS:
+1. String: Chuỗi
+2. Number: Số
+3. Object: Đối tượng
+4. Boolean: true/false
+5. undefinded: Không xác định (let a;)
+6. null: Rỗng (Bài toán chủ nhà trọ ....)
+
+Toán tử: 
+1. Toán tử gán: =
+2. Toán tử số học: +,-,*,/,% (chia lấy dư)
+3. Toán tử so sánh: >,<,>=, <=, == (So sánh tương đối), === (so sánh tuyệt đối)
+4. Toán tử logic: && (và và ), || (hoặc hoặc)
+5. Toán tử 3 ngôi:  (điều kiện)?"Câu lệnh 1":"Câu lệnh 2" (Tương đương if else)
+
+
+Khai báo biến: let, var, const (khai báo hằng số => không thể thay đổi được)
+*/
+
+
+////Kiểu dữ liệu
+
+// String
+let a = "Hello";
+let b = 'Hello';
+let c = `Hello`;
