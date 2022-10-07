33506fb427de21d154ee272b2593dedb3775085a
hieu1966hn <hieu1966hn@gmail.com>
2022-10-07 21:21:07 +0700

    new code btvn

--- SS2/ss3.js
+++ SS2/ss3.js
@@ -76,10 +76,51 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]/// => 2,4,6,... 20
 
 
 //// Cách 2: Duyệt mảng + thực hành điều kiện với map => kết quả trả về của map là 1 mảng mới
-let newArr = arr.map(item => item * 2); // 2,4, .....,20
-console.log("newArr: ", newArr);
+// let newArr = arr.map(item => item * 2); // 2,4, .....,20
+// console.log("newArr: ", newArr);
 
 
 ///// In ra mảng có phần tử bên trong nhỏ hơn phần tử mảng arr ban đầu 1 giá trị là 5
-let newArr5 = arr.map(x => x - 5);// (x) => {return x-5} || x => x - 5
-console.log("newArr5: ", newArr5);
+// let newArr5 = arr.map(x => x - 5);// (x) => {return x-5} || x => x - 5
+// console.log("newArr5: ", newArr5);
+
+
+
+
+////// Lấy ra các phần tử nhỏ hơn 5 ra 1 mảng mới  và in ra trên màn hình console (Filter)
+let newArr = arr.filter(item => item < 5);
+console.log("newArr: ", newArr);
+
+
+/*
+KL
+- map: Dùng để thực thi câu lệnh với từng phần tử mảng
+- filter: Dùng để lọc/lấy ra phần tử mảng thỏa mãn điều kiện đặt ra
+- indexOf: Dùng để trả về vị trí phần tử mảng muốn tìm
+- Find: Dùng để trả về giá trị phần tử muốn tìm trong mảng
+*/
+
+let animals = [
+  {
+    name: "Cat",
+    size: "small",
+    weight: 5
+  },
+  {
+    name: "dog",
+    size: "small",
+    weight: 10
+  },
+  {
+    name: "pig",
+    size: "big",
+    weight: 200
+  },
+]
+/*
+Bài toán: 
+1. In ra tên từng con vật trong mảng animals
+2. Lấy ra con vật nào có cân nặng < 100
+3. Lọc/lấy ra con vật nào có cân nặng nhỏ và in ra tên con vật đó.
+*/
+
