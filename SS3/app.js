ddae7d7101f6ca3b8c3998b07f76d243791edb71
hieu1966hn <hieu1966hn@gmail.com>
2022-10-14 21:02:33 +0700

    new code bài 2

--- SS3/app.js
+++ SS3/app.js
@@ -1,18 +1,30 @@
 /*
-Bài 1: 
-Cho 2 mảng sau: arr1 = [1,2,4,5,6]; arr2 = [1.6.8.9.0]. 
+Bài 1:
+Cho 2 mảng sau: arr1 = [1,2,4,5,6]; arr2 = [1.6.8.9.0].
 Hãy lọc ra một mảng mới chứa 2 phần tử trùng nhau của 2 mảng cho bên trên. (sử dụng filter() )
 */
 
-let arr1 = [1, 2, 4, 5, 6];
-let arr2 = [1, 6, 8, 9, 0];
+// let arr1 = [1, 2, 4, 5, 6];
+// let arr2 = [1, 6, 8, 9, 0];
 
-//// Giới thiệu method includes => trả về true/false khi kiểm tra phần tử đó có trong mảng hay không VD
-console.log(
-  arr1.includes(10)
-); // => 
+// //// Giới thiệu method includes => trả về true/false khi kiểm tra phần tử đó có trong mảng hay không VD
+// console.log(
+//   arr1.includes(10)
+// ); // => 
 
-//// Giải bài 1
+// //// Giải bài 1
+// console.log(
+//   arr1.filter(item => arr2.includes(item))
+// );
+
+
+/*
+Bài 02: Sử dụng map() với arr = [1,54,6,7] 
+để tạo ra một newArr có newArr[i] = arr[i] + 5.
+
+*/
+let arr = [1, 54, 6, 7]
+let newArr = arr.map(item => item + 5)
 console.log(
-  arr1.filter(item => arr2.includes(item))
+  newArr
 );
