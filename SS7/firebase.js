d43aae81b2ac1067c12de753b06452854085fa2c
hieu1966hn <hieu1966hn@gmail.com>
2022-11-11 21:03:46 +0700

    New code firebase.js

--- SS7/firebase.js
+++ SS7/firebase.js
@@ -1,4 +1,5 @@
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { collection, getDocs, getFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 
 // Your web app's Firebase configuration
@@ -16,3 +17,62 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 console.log("db: ", db);
 // db.settings({ timestampsInSnapshóts: true })
+
+
+const cafeList = document.getElementById("cafe-list");
+const form = document.getElementById("add-cafe-form");
+
+
+
+//// Create element & render cafe
+function renderCafe(doc) {
+  let li = document.createElement("li");
+  let name = document.createElement("span");
+  let city = document.createElement("span");
+  let cross = document.createElement("div");
+
+  li.setAttribute("data-id", doc.id);
+  name.textContent = doc.data().name;
+  city.textContent = doc.data().city;
+  cross.textContent = "x";
+
+
+  li.appendChild(name);
+  li.appendChild(city);
+  li.appendChild(cross);
+
+  cafeList.appendChild(li);
+
+  ///deleting data
+  cross.addEventListener("submit", (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute("data-id");
+    db.collection("cafes")
+      .doc(id)
+      .delete();
+  })
+}
+
+
+///// saving data
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  console.log(
+    getCafe(db)
+
+  );
+
+  db.collection("cafes").add({
+    name: form.name.value,
+    city: form.city.value
+  })
+})
+
+async function getCafe(db) {
+  const cafes = collection(db, 'cafes');
+  const cafesSnapshot = await getDocs(cafes);
+  const cafeList = cafesSnapshot.docs.map(doc => doc.data());
+  return cafeList;
+}
+
